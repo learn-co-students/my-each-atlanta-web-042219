10f56f014249bf910c7956f354f8cf440e637629
github username <github email address>
2019-03-14 15:55:13 -0400

    done

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,31 @@
-def my_each # put argument(s) here
-  # code here
+def my_each(array)
+  i = 0
+  while i < array.length
+    yield array[i]
+    i += 1
+  end
+  return array
 end
+
+
+
+
+
+# def my_each(array)
+#   i = 1
+#   while i < 5
+#   array.times do |number| i = i + 1
+#   end
+# end
+
+
+
+
+# def my_each(array)
+#   i = 0
+#   while i < array.length
+# # The "while" method will always return a value of => nil"
+#     yield(array[i])
+#     i = i + 1
+#   end
+# end
