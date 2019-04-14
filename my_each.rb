fc8dd5d3620fb1b4dbad34ee9a5a0ecd2eb4874d
github username <github email address>
2019-04-14 08:41:20 -0400

    yield for each

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,10 @@
-def my_each # put argument(s) here
-  # code here
+def my_each(array) # put argument(s) here
+    x = 0
+
+  while x < array.length
+    yield array[x]
+    x += 1
+     x # code here
+  end
+  array
 end
